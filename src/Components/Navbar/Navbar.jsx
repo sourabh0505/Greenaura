b75fbc490168e17a4d4a569d0f73fee3f5b7c813
sourabh0505 <sourabhvishnoi05@gmail.com>
2023-10-24 00:16:01 +0530

    navbar updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ function Navbar() {
                 <a className='list-item' href='/' spy={true} smooth={true} offset={-1500} duration={1200}>Home</a>
                 <Link  className='list-item' to="about" spy={true} smooth={true} offset={-80} duration={1200}>About</Link>
                 {/* <Link to="blogs" spy={true} smooth={true} offset={50} duration={1200} className='list-item'>Blogs</Link> */}
-                <Link  className='list-item' to="courses" spy={true} smooth={true} offset={-30} duration={1200}>Quizzes</Link>
+                <Link  className='list-item' to="courses" spy={true} smooth={true} offset={-30} duration={1200}>Courses</Link>
                 <Link  className='list-item' to="gallery" spy={true} smooth={true} offset={-60} duration={1200}>Gallery</Link>
                 <Link  className='list-item' to="contact" spy={true} smooth={true} offset={50} duration={1200}>Contact</Link>
                 <a  className='list-item' href='/books'>Books</a>
