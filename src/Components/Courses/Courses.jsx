bf4e868ee4e839c4d164da5272deed508d641ed5
sourabh0505 <sourabhvishnoi05@gmail.com>
2023-10-23 23:34:51 +0530

    course and quiz btn added and updated

--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -12,7 +12,10 @@ return (
         <div className="coursecardbody">
             <h3 className='course-title'>Fundamentals of Renewable energy Quiz</h3>
             <p className='course-text'>Learn about fundamentals of renewable energy. If you are a novice the this short course is a perfect stepping stone to build career in the green future...</p>
-            <a target='_blank' href="https://docs.google.com/forms/d/e/1FAIpQLScTV7r8YyHE_RC-ovL4lmTLghMLRYdrbKjeaXx-kTNka-49bQ/viewform?vc=0&c=0&w=1&flr=0"><button className='course-btn'>Start the Quiz</button></a>
+            <div className="course-quiz-btn">
+            <a target='_blank' href="https://drive.google.com/file/d/1YD_1_IRJw2f7HmUmlx64sH2ZlrWcTDuv/view"><button className='course-btn'>Learn More</button></a>
+            <a target='_blank' href="https://docs.google.com/forms/d/e/1FAIpQLScTV7r8YyHE_RC-ovL4lmTLghMLRYdrbKjeaXx-kTNka-49bQ/viewform?vc=0&c=0&w=1&flr=0"><button className='quiz-btn'>Start the Quiz</button></a>
+            </div>
         </div>
     </div>
 
@@ -21,7 +24,10 @@ return (
         <div className="coursecardbody">
             <h3 className='course-title'>Photovoltaics Systems and Smart Grids Quiz</h3>
             <p className='course-text'>If you have a question that how light is actually converted into electricity and what is the footprint of photovoltaic systems then this course will go through all information you need to understand about solar energy...</p>
-            <a target='_blank' href="https://docs.google.com/forms/d/e/1FAIpQLSe3gu3lsVoNL_oAeLE9BwCtA4wmnV5KE-P4ObKSvfVQf2D7WQ/viewform?vc=0&c=0&w=1&flr=0"><button className='course-btn'>Start the Quiz</button></a>
+            <div className="course-quiz-btn">
+            <a target='_blank' href="https://drive.google.com/file/d/1YGz3R066043jzolnplkg6ciqUOToOhpA/view"><button className='course-btn'>Learn More</button></a>
+            <a target='_blank' href="https://docs.google.com/forms/d/e/1FAIpQLSe3gu3lsVoNL_oAeLE9BwCtA4wmnV5KE-P4ObKSvfVQf2D7WQ/viewform?vc=0&c=0&w=1&flr=0"><button className='quiz-btn'>Start the Quiz</button></a>
+            </div>
 
         </div>
     </div>
@@ -31,7 +37,10 @@ return (
         <div className="coursecardbody">
             <h3 className='course-title'>Introduction to Electric Vehicles Quiz</h3>
             <p className='course-text'>Excited about transition of automobile industry and want basic technical knowledge of electric vehicles? Check out this mini course. Tailored for beginners who are passionate about working of such marvellous machines...</p>
-            <a target='_blank' href="https://www.google.com/url?q=https://docs.google.com/forms/d/e/1FAIpQLSesXMoXPdSSE2SMaiHy4iThJjvpC9SkezIuFxIQX1C3C0mgZA/viewform?vc%3D0%26c%3D0%26w%3D1%26flr%3D0%26usp%3Dmail_form_link&source=gmail&ust=1697649062756000&usg=AOvVaw0sxc5HKrle67fuoQWLukaR"><button className='course-btn'>Start the Quiz</button></a>
+            <div className="course-quiz-btn">
+            <a target='_blank' href="https://drive.google.com/file/d/1YHvDAv3Gb1qF8YNxzT6mIYy7xXbN6cT0/view?usp=drivesdk"><button className='course-btn'>Learn More</button></a>
+            <a target='_blank' href="https://www.google.com/url?q=https://docs.google.com/forms/d/e/1FAIpQLSesXMoXPdSSE2SMaiHy4iThJjvpC9SkezIuFxIQX1C3C0mgZA/viewform?vc%3D0%26c%3D0%26w%3D1%26flr%3D0%26usp%3Dmail_form_link&source=gmail&ust=1697649062756000&usg=AOvVaw0sxc5HKrle67fuoQWLukaR"><button className='quiz-btn'>Start the Quiz</button></a>
+            </div>
         </div>
     </div>
     </div>
